7970584230330650c2434079854fa58193014d7e
kyne <Hammerhead12->
2020-02-12 21:27:28 +0100

    Update Readme. Fixed publication to work in production

--- server/imports/publications/categories.ts
+++ server/imports/publications/categories.ts
@@ -2,11 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import { Categories } from '../../../imports/collections/categories';
 import { buildItemQuery, buildUserQuery } from './query-utils';
 
-Meteor.publish('categories', () => {
-  const selector = buildUserQuery(this.Meteor.userId());
+Meteor.publish('categories', function() {
+  const selector = buildUserQuery(this.userId);
   return Categories.find(selector);
 });
 
-Meteor.publish('category', (categoryId: string) => {
-  return Categories.find(buildItemQuery(this.Meteor.userId(), categoryId));
+Meteor.publish('category', function(categoryId: string) {
+  return Categories.find(buildItemQuery(this.userId, categoryId));
 });
